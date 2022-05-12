53df667f4d9e3078eac895b0163fc71e57bb8efa
Zennoune Karim <kzennoun@student.42lyon.fr>
2022-05-12 16:56:40 +0200

    poll first batch WIP

--- srcs/test.cpp
+++ srcs/test.cpp
@@ -6,14 +6,17 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 13:50:38 by kzennoun          #+#    #+#             */
-/*   Updated: 2022/05/12 15:31:35 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2022/05/12 16:56:01 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+
 #include "../includes/ft_irc.hpp"
 
- #include <fcntl.h>
+#include <fcntl.h>
 #include <vector>
+#include <cstdio>
 
 #define TRUE             1
 #define FALSE            0
@@ -21,11 +24,14 @@
 int main(int ac, char **av)
 {
 
-	int							socketFD, timeout;
-	int							on, ret = 1;
+	int							socketFD, clientFD, timeout;
+	int							len, on, ret = 1;
 	struct sockaddr_in			addr;
 	std::vector<struct pollfd>	fds;
 	struct pollfd				fd;
+	bool						end_server = FALSE;
+	bool						close_conn = FALSE;
+	char						buffer[2048];
 
 	if (ac < 2 || ac > 3)
 	{
@@ -101,10 +107,214 @@ int main(int ac, char **av)
 
 
 
+/*************************************************************/
+  /* Loop waiting for incoming connects or for incoming data   */
+  /* on any of the connected sockets.                          */
+  /*************************************************************/
+	do
+	{
+		// printf("Waiting on poll()...\n");
+		std::cout << "Waiting on poll()..." << std::endl;
+		ret = poll(&(fds[0]), fds.size(), timeout);
+		if (ret < 0)
+		{
+			perror("  poll() failed");
+			break;
+		}
+		
+		if (ret == 0)
+		{
+			std::cerr << "poll() timed out.  End program." << std::endl;
+			break;
+		}
+
+		for (int i = 0; i < fds.size(); i++)
+		{
+			if(fds[i].revents == 0)
+				continue;
+
+			if(fds[i].revents != POLLIN)
+			{
+				std::cerr << "Error! revents = " << fds[i].revents << std::endl;
+				end_server = TRUE;
+				break;
+
+			}
+			
+			if (fds[i].fd == socketFD)
+			{
+				do
+				{
+					/*****************************************************/
+					/* Accept each incoming connection. If               */
+					/* accept fails with EWOULDBLOCK, then we            */
+					/* have accepted all of them. Any other              */
+					/* failure on accept will cause us to end the        */
+					/* server.                                           */
+					/*****************************************************/
+					clientFD = accept(socketFD, NULL, NULL);
+					if (clientFD < 0)
+					{
+						if (errno != EWOULDBLOCK)
+						{
+							perror("  accept() failed");
+							end_server = TRUE;
+						}
+						break;
+					}
+
+					/*****************************************************/
+					/* Add the new incoming connection to the            */
+					/* pollfd structure                                  */
+					/*****************************************************/
+					//printf("  New incoming connection - %d\n", new_sd);
+					std::cout << "New incomig connection: " << clientFD << std::endl;
+
+					fd.fd = clientFD;
+					fd.events = POLLIN;
+					fds.push_back(fd);
+					// fds[nfds].fd = new_sd;
+					// fds[nfds].events = POLLIN;
+					// nfds++;
+
+					/*****************************************************/
+					/* Loop back up and accept another incoming          */
+					/* connection                                        */
+					/*****************************************************/
+				} while (clientFD != -1);
+			}
+			else
+			{
+				//printf("  Descriptor %d is readable\n", fds[i].fd);
+				std::cout << "Descriptor " << fds[i].fd << " is readable" << std::endl;
+				close_conn = FALSE;
+				/*******************************************************/
+				/* Receive all incoming data on this socket            */
+				/* before we loop back and call poll again.            */
+				/*******************************************************/
+
+				do
+				{
+					/*****************************************************/
+					/* Receive data on this connection until the         */
+					/* recv fails with EWOULDBLOCK. If any other         */
+					/* failure occurs, we will close the                 */
+					/* connection.                                       */
+					/*****************************************************/
+					ret = recv(fds[i].fd, buffer, sizeof(buffer), 0);
+					if (ret < 0)
+					{
+						if (errno != EWOULDBLOCK)
+						{
+							perror("  recv() failed");
+							close_conn = TRUE;
+						}
+						break;
+					}
+
+					/*****************************************************/
+					/* Check to see if the connection has been           */
+					/* closed by the client                              */
+					/*****************************************************/
+					if (ret == 0)
+					{
+						printf("  Connection closed\n");
+						close_conn = TRUE;
+						break;
+					}
+
+					/*****************************************************/
+					/* Data was received                                 */
+					/*****************************************************/
+					len = ret;
+					std::cout << len << " bytes received " << std::endl;
+
+
+
+
+
+
+
+
+
+
+					//change that part ?
+					/*****************************************************/
+					/* Echo the data back to the client                  */
+					/*****************************************************/
+					ret = send(fds[i].fd, buffer, len, 0);
+					if (ret < 0)
+					{
+						perror("  send() failed");
+						close_conn = TRUE;
+						break;
+					}
+
+
+
+
+
+
+
+
+				} while(TRUE);
+
+				/*******************************************************/
+				/* If the close_conn flag was turned on, we need       */
+				/* to clean up this active connection. This            */
+				/* clean up process includes removing the              */
+				/* descriptor.                                         */
+				/*******************************************************/
+				if (close_conn)
+				{
+					close(fds[i].fd);
+					fds[i].fd = -1;
+				//	compress_array = TRUE;
+				}
+
+
+			}  /* End of existing connection is readable             */
+		} /* End of loop through pollable descriptors              */
+
+		/***********************************************************/
+		/* If the compress_array flag was turned on, we need       */
+		/* to squeeze together the array and decrement the number  */
+		/* of file descriptors. We do not need to move back the    */
+		/* events and revents fields because the events will always*/
+		/* be POLLIN in this case, and revents is output.          */
+		/***********************************************************/
+
+			for (int i = 0; i < fds.size(); i++)
+			{
+				if (fds[i].fd == -1)
+				{
+					fds.erase(fds.begin() + i);
+				}
+			}
+		//}
+
+		} while (end_server == FALSE); /* End of serving running.    */
+
+		/*************************************************************/
+		/* Clean up all of the sockets that are open                 */
+		/*************************************************************/
+		for (int i = 0; i < fds.size(); i++)
+		{
+			if(fds[i].fd >= 0)
+				close(fds[i].fd);
+		}
+}
+
+
+
+
+
+
+
 
 //std::vector<struct pollfd> fds;
 //int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
-}
+//}
 
 /*
 
