15c5031e64df6b2daaf4c7c2c094b268059e55dc
Romain Zhou <rozhou@z1r8p4.42lyon.fr>
2022-05-18 14:47:54 +0200

    STRINGIFY modified

--- includes/numerics.hpp
+++ includes/numerics.hpp
@@ -2,7 +2,6 @@
 # define    NUMERICS_HPP
 
 #define NETWORK_NAME "KIKIKAKARORO"
-#define STRINGIFY(x) #x
 
 //												RPL MESSAGES												//
 
@@ -27,11 +26,11 @@
 #define RPL_LOCALUSERS(u, m)(" " + u + " " + m + " :Current local users " + u + ", max " + m + "\n")																															// 265
 #define RPL_GLOBALUSERS(u, m)(" " + u + " " + m + " :Current global users " + u + ", max " + m + "\n")																															// 266
 #define RPL_WHOISCERTFP(nick, fingerprint)(" " + nick + " :has client certificate fingerprint " + fingerprint + "\n")																											// 276
-#define RPL_NONE()("Undefined format" + "\n")																																													// 300
+#define RPL_NONE(NULL)(NULL + "Undefined format" + "\n")																																													// 300
 #define RPL_AWAY(nick, message)(" " + nick + " :" + message + "\n")																																							// 301
 #define RPL_USERHOST(reply)(" :" + reply + "\n")																																												// 302
-#define RPL_UNAWAY()(" :You are no longer marked as being away" + "\n")																																						// 305
-#define RPL_NOWAWAY()(" :You have been marked as being away" + "\n")																																							// 306
+#define RPL_UNAWAY(NULL)(NULL + " :You are no longer marked as being away" + "\n")																																						// 305
+#define RPL_NOWAWAY(NULL)(NULL + " :You have been marked as being away" + "\n")																																							// 306
 #define RPL_WHOREPLY(channel, username, host, server, nick, flags, hopcount, realname)(" " + channel + " " + username + " " + host + " " + server + " " + nick + " " + flags + " :" + hopcount + " " + realname + "\n")		// 352
 #define RPL_ENDOFWHO(mask)(" " + mask + " :End of WHO list" + "\n")																																							// 315
 #define RPL_WHOISREGNICK(nick)(" " + nick + " :has identified for this nick" + "\n")																																			// 307
@@ -43,9 +42,9 @@
 #define RPL_ENDOFWHOIS(nick)(" " + nick + " :End of /WHOIS list" + "\n")																																						// 318
 #define RPL_WHOISCHANNELS(nick, channel)(" " + nick + " :" + channel + "\n")																																					// 319
 #define RPL_WHOISSPECIAL(nick)(" " + nick + " :blah blah blah" + "\n")																																							// 320
-#define RPL_LISTSTART()(" " + "Channel" + " :Users  Name" + "\n")					// SKIPPED BY THE SERVER !																													// 321
+#define RPL_LISTSTART(NULL)(NULL + " " + "Channel" + " :Users  Name" + "\n")					// SKIPPED BY THE SERVER !																													// 321
 #define RPL_LIST(channel, client_count, topic)(" " + channel + " " + client_count + " :" + topic + "\n")																														// 322
-#define RPL_LISTEND()(" :End of /LIST" + "\n")																																													// 323
+#define RPL_LISTEND(NULL)(NULL + " :End of /LIST" + "\n")																																													// 323
 #define RPL_CHANNELMODEIS(channel, modestring, mode_arguments)(" " + channel + " " + modestring + " " + mode_arguments + "\n")																									// 324
 #define RPL_CREATIONTIME(channel, creationtime)(" " + channel + " " + creationtime + "\n")																																		// 329
 #define RPL_WHOISACCOUNT(nick, account)(" " + nick + " " + account + " :is logged in as" + "\n")																																// 330
@@ -53,7 +52,7 @@
 #define RPL_TOPIC(channel, topic)(" " + channel + " :" + topic + "\n")																																							// 332
 #define RPL_TOPICWHOTIME(channel, nick, setat)(" " + channel + " " + nick + " " + setat + "\n")																																// 333
 #define RPL_INVITELIST(channel)(" " + channel + "\n")																																											// 336
-#define RPL_ENDOFINVITELIST()(" :End of /INVITE list" + "\n")																																									// 337
+#define RPL_ENDOFINVITELIST(NULL)(NULL + " :End of /INVITE list" + "\n")																																									// 337
 //338 ???
 #define RPL_INVITING(nick, channel)(" " + nick + " " + channel + "\n")																																							// 341
 #define RPL_INVEXLIST(channel, mask)(" " + channel + " " + mask + "\n")																																						// 346
@@ -64,28 +63,28 @@
 #define RPL_NAMREPLY(symbol, channel, nick)(" " + symbol + " " + channel + " :" + nick + "\n")																																	// 353
 #define RPL_ENDOFNAMES(channel)(" " + channel + " :End of /NAMES list" + "\n")																																					// 366
 #define RPL_LINKS(server, hopcount, server_info)(" * " + server + " :" + hopcount + " " + server_info + "\n")																													// 364
-#define RPL_ENDOFLINKS()(" * " + " :End of /LINKS list" + "\n")																																								// 365
+#define RPL_ENDOFLINKS(NULL)(NULL + " * " + " :End of /LINKS list" + "\n")																																								// 365
 #define RPL_BANLIST(channel, mask, who, set_ts)(" " + channel + " " + mask + " " + who +  " " + set_ts + "\n")																													// 367
 #define RPL_ENDOFBANLIST(channel)(" " + channel + " :End of channel ban list" + "\n")																																			// 368
 #define RPL_ENDOFWHOWAS(nick)(" " + nick + " :End of WHOWAS" + "\n")																																							// 369
 #define RPL_INFO(string)(" :" + string + "\n")																																													// 371
-#define RPL_ENDOFINFO()(" :End of INFO list" + "\n")																																											// 374
+#define RPL_ENDOFINFO(NULL)(NULL + " :End of INFO list" + "\n")																																											// 374
 #define RPL_MOTDSTART(server)(" :- " + server + " Message of the day - " + "\n")																																				// 375
 #define RPL_MOTD(motd)(" :" + motd + "\n")																																														// 372
-#define RPL_ENDOFMOTD()(" :End of /MOTD command." + "\n")																																										// 376
+#define RPL_ENDOFMOTD(NULL)(NULL + " :End of /MOTD command." + "\n")																																										// 376
 #define RPL_WHOISHOST(nick)(" " + nick + " :is connecting from *@localhost 127.0.0.1" + "\n")																																	// 378
 #define RPL_WHOISMODES(nick)(" " + nick + " :is using modes +ailosw" + "\n")																																					// 379
-#define RPL_YOUREOPER()(" :You are now an IRC operator" + "\n")																																								// 381
+#define RPL_YOUREOPER(NULL)(NULL + " :You are now an IRC operator" + "\n")																																								// 381
 #define RPL_REHASHING(config_file)(" " + config_file + " :Rehashing" + "\n")																																					// 382
 #define RPL_TIME(server, time)(" " + server + " :" + time + "\n")																																								// 391
-#define RPL_STARTTLS()(" :STARTTLS successful, proceed with TLS handshake" + "\n")			//Ici																																	// 670									
+#define RPL_STARTTLS(NULL)(NULL + " :STARTTLS successful, proceed with TLS handshake" + "\n")			//Ici																																	// 670									
 #define RPL_WHOISSECURE(nick)(" " + nick + " :is using a secure connection" + "\n")																																			// 671						
 #define RPL_HELPSTART(subject, str)(" " + subject + " :" + str + "\n")																																							// 704
 #define RPL_HELPTXT(subject, str)(" " + subject + " :" + str + "\n")																																							// 705
 #define RPL_ENDOFHELP(subject, str)(" " + subject + " :" + str + "\n")																																							// 706
 #define RPL_LOGGEDIN(nick, user, host, account, username)(" " + nick + "!" + user + "@" + host + " " + account + " :You are now logged in as " + username + "\n")																// 900																						
 #define RPL_LOGGEDOUT(nick, user, host, account)(" " + nick + "!" + user + "@" + host + " " + account + " :You are now logged out" + "\n")																						// 901																	
-#define RPL_SASLSUCCESS()(" :SASL authentication successful" + "\n")																																							// 903
+#define RPL_SASLSUCCESS(NULL)(NULL + " :SASL authentication successful" + "\n")																																							// 903
 #define RPL_SASLMECHS(mechanisms)(" " + mechanisms + " :are available SASL mechanisms" + "\n")																																	// 908					
 
 //												ERROR MESSAGES												//
@@ -96,42 +95,42 @@
 #define ERR_NOSUCHCHANNEL(channel) (" " + channel + " :No such channel" + "\n")																																				// 403						
 #define ERR_CANNOTSENDTOCHAN(channel)(" " + channel + " :Cannot send to channel" + "\n")																																		// 404								
 #define ERR_TOOMANYCHANNELS(channel)(" " + channel + " :You have joined too many channels" + "\n")																																// 405										
-#define ERR_WASNOSUCHNICK() (" :There was no such nickname" + "\n")																																								// 406		
-#define ERR_NOORIGIN()(" :No origin specified" + "\n")																																											// 409
-#define ERR_INPUTTOOLONG ()(" :Input line was too long" + "\n")																																									// 417	
+#define ERR_WASNOSUCHNICK(NULL)(NULL + " :There was no such nickname" + "\n")																																								// 406		
+#define ERR_NOORIGIN(NULL)(NULL + " :No origin specified" + "\n")																																											// 409
+#define ERR_INPUTTOOLONG(NULL)(NULL + " :Input line was too long" + "\n")																																									// 417	
 #define ERR_UNKNOWNCOMMAND(command)(" " + command + " :Unknown command" + "\n")																																				// 421							
-#define ERR_NOMOTD()(" :MOTD File is missing" + "\n")																																											// 422
+#define ERR_NOMOTD(NULL)(NULL + " :MOTD File is missing" + "\n")																																											// 422
 #define ERR_ERRONEUSNICKNAME(nickname)(" " + nickname + " :Erroneus nickname" + "\n")																																			// 432								
 #define ERR_NICKNAMEINUSE(nickname)(" " + nickname + " :Nickname is already in use" + "\n")																																	// 433										
 #define ERR_USERNOTINCHANNEL(nickname, channel)(" " + nickname + " " + channel + " :They aren't on that channel" + "\n")																										// 441																	
 #define ERR_NOTONCHANNEL(channel)(" " + channel + " :You're not on that channel" + "\n")																																		// 442									
 #define ERR_USERONCHANNEL(nickname, channel)(" " + nickname + " " + channel + " :is already on channel" + "\n")																												// 443															
-#define ERR_NOTREGISTERED()(" :You have not registered" + "\n")																																								// 451			
+#define ERR_NOTREGISTERED(NULL)(NULL + " :You have not registered" + "\n")																																								// 451			
 #define ERR_NEEDMOREPARAMS(command)(" " + command + " :Not enough parameters" + "\n")																																			// 461								
-#define ERR_ALREADYREGISTERED()(" :You may not reregister" + "\n")																																						// 462					
-#define ERR_PASSWDMISMATCH()(" :Password incorrect" + "\n")																																									// 464		
-#define ERR_YOUREBANNEDCREEP()(" :You are banned from this server." + "\n")																																					// 465						
+#define ERR_ALREADYREGISTERED(NULL)(NULL + " :You may not reregister" + "\n")																																						// 462					
+#define ERR_PASSWDMISMATCH(NULL)(NULL + " :Password incorrect" + "\n")																																									// 464		
+#define ERR_YOUREBANNEDCREEP(NULL)(NULL + " :You are banned from this server." + "\n")																																					// 465						
 #define ERR_CHANNELISFULL(channel)(" " + channel + " :Cannot join channel (+l)" + "\n")																																		// 471									
 #define ERR_UNKNOWNMODE(modechar)(" " + modechar + " :is unknown mode char to me" + "\n")																																		// 472									
 #define ERR_INVITEONLYCHAN(channel)(" " + channel + " :Cannot join channel (+i)" + "\n")																																		// 473									
 #define ERR_BANNEDFROMCHAN(channel)(" " + channel + " :Cannot join channel (+b)" + "\n")																																		// 474									
 #define ERR_BADCHANNELKEY(channel)(" " + channel + " :Cannot join channel (+k)" + "\n")																																		// 475									
 #define ERR_BADCHANMASK(channel)(" " + channel + " :Bad Channel Mask" + "\n")																																					// 476						
-#define ERR_NOPRIVILEGES()(" :Permission Denied- You're not an IRC operator" + "\n")																																			// 481								
+#define ERR_NOPRIVILEGES(NULL)(NULL + " :Permission Denied- You're not an IRC operator" + "\n")																																			// 481								
 #define ERR_CHANOPRIVSNEEDED(channel)(" " + channel + " :You're not channel operator" + "\n")																																	// 482										
-#define ERR_CANTKILLSERVER()(" :You cant kill a server!" + "\n")																																								// 483			
-#define ERR_NOOPERHOST()(" :No O-lines for your host" + "\n")																																									// 491		
-#define ERR_UMODEUNKNOWNFLAG()(" :Unknown MODE flag" + "\n")																																									// 501		
-#define ERR_USERSDONTMATCH()(" :Cant change mode for other users" + "\n")																																						// 502					
+#define ERR_CANTKILLSERVER(NULL)(NULL + " :You cant kill a server!" + "\n")																																								// 483			
+#define ERR_NOOPERHOST(NULL)(NULL + " :No O-lines for your host" + "\n")																																									// 491		
+#define ERR_UMODEUNKNOWNFLAG(NULL)(NULL + " :Unknown MODE flag" + "\n")																																									// 501		
+#define ERR_USERSDONTMATCH(NULL)(NULL + " :Cant change mode for other users" + "\n")																																						// 502					
 #define ERR_HELPNOTFOUND(subject)(" " + subject + " :No help available on this topic" + "\n")																																	// 524										
 #define ERR_INVALIDKEY(target_chan)(" " + target_chan + " :Key is not well-formed" + "\n")																																		// 525									
-#define ERR_STARTTLS()(" :STARTTLS failed (Wrong moon phase)" + "\n")																																					// 691						
+#define ERR_STARTTLS(NULL)(NULL + " :STARTTLS failed (Wrong moon phase)" + "\n")																																					// 691						
 #define ERR_INVALIDMODEPARAM(target, mode, parameter, description)(" " + target, " " + mode + " " + parameter + " :" + description + "\n")																						// 696																					
 #define ERR_NOPRIVS(priv)(" " + priv + " :Insufficient oper privileges." + "\n")																																				// 723							
-#define ERR_NICKLOCKED()(" :You must use a nick assigned to you" + "\n")																																						// 902					
-#define ERR_SASLFAIL()(" :SASL authentication failed" + "\n")																																									// 904		
-#define ERR_SASLTOOLONG()(" :SASL message too long" + "\n")																																									// 905		
-#define ERR_SASLABORTED()(" :SASL authentication aborted" + "\n")																																								// 906			
-#define ERR_SASLALREADY()(" :You have already authenticated using SASL" + "\n")																																				// 907							
+#define ERR_NICKLOCKED(NULL)(NULL + " :You must use a nick assigned to you" + "\n")																																						// 902					
+#define ERR_SASLFAIL(NULL)(NULL + " :SASL authentication failed" + "\n")																																									// 904		
+#define ERR_SASLTOOLONG(NULL)(NULL + " :SASL message too long" + "\n")																																									// 905		
+#define ERR_SASLABORTED(NULL)(NULL + " :SASL authentication aborted" + "\n")																																								// 906			
+#define ERR_SASLALREADY(NULL)(NULL + " :You have already authenticated using SASL" + "\n")																																				// 907							
 
 #endif
--- srcs/message.cpp
+++ srcs/message.cpp
@@ -84,6 +84,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
 	//update 16/05
 	std::string	help_msg = "Help me";			//May differ if it's the start/mid/end of the help section
 	std::string description = "Description";
+    std::string null = "";
 
     switch (nb_message)
     {
@@ -148,7 +149,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_WHOISCERTFP(user_nick, user_fingerprint);
         break;
     case 300:
-        std::cout << username << STRINGIFY(RPL_NONE());
+        std::cout << username << RPL_NONE(null);
         break;
     case 301:
         std::cout << username << RPL_AWAY(user_nick, away_message);
@@ -157,10 +158,10 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_USERHOST(command);         //NEED TO CREATE A MESSAGE cf: https://modern.ircdocs.horse/#rpluserhost-302
         break;
     case 305:
-        std::cout << username << STRINGIFY(RPL_UNAWAY());
+        std::cout << username << RPL_UNAWAY(null);
         break;
     case 306:
-        std::cout << username << STRINGIFY(RPL_NOWAWAY());
+        std::cout << username << RPL_NOWAWAY(null);
         break;
     case 352:
         std::cout << username << RPL_WHOREPLY(channel_name, username, hostname, servername, user_nick, user_flags, hopcount, realname);
@@ -197,13 +198,13 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_WHOISSPECIAL(user_nick);
         break;
     case 321:
-        std::cout << username << STRINGIFY(RPL_LISTSTART());
+        std::cout << username << RPL_LISTSTART(null);
         break;
     case 322:
         std::cout << username << RPL_LIST(channel_name, client_count, channel_topic);
         break;
     case 323:
-        std::cout << username << STRINGIFY(RPL_LISTEND());
+        std::cout << username << RPL_LISTEND(null);
         break;
     case 324:
         std::cout << username << RPL_CHANNELMODEIS(channel_name, mode_string, mode_arguments);
@@ -227,7 +228,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_INVITELIST(channel_name);
         break;
     case 337:
-        std::cout << username << STRINGIFY(RPL_ENDOFINVITELIST());
+        std::cout << username << RPL_ENDOFINVITELIST(null);
         break;
     case 341:
         std::cout << username << RPL_INVITING(user_nick, channel_topic);
@@ -257,7 +258,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_LINKS(servername, hopcount, infoServer);
         break;
     case 365:
-        std::cout << username << STRINGIFY(RPL_ENDOFLINKS());
+        std::cout << username << RPL_ENDOFLINKS(null);
         break;
     case 367:
         std::cout << username << RPL_BANLIST(channel_name, banMask, banner_nick, ban_timeSet);           //
@@ -272,7 +273,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_INFO(infoServer);          // Perharps other info
         break;
     case 374:
-        std::cout << username << STRINGIFY(RPL_ENDOFINFO());
+        std::cout << username << RPL_ENDOFINFO(null);
         break;
     case 375:
         std::cout << username << RPL_MOTDSTART(servername);
@@ -281,7 +282,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_MOTD(motd);
         break;
     case 376:
-        std::cout << username << STRINGIFY(RPL_ENDOFMOTD());
+        std::cout << username << RPL_ENDOFMOTD(null);
         break;
     case 378:
         std::cout << username << RPL_WHOISHOST(user_nick);          //Need to define ip ?
@@ -290,7 +291,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_WHOISMODES(user_nick);         //Need to define available modes ?
         break;
     case 381:
-        std::cout << username << STRINGIFY(RPL_YOUREOPER());
+        std::cout << username << RPL_YOUREOPER(null);
         break;
     case 382:
         std::cout << username << RPL_REHASHING(Rehashing_message);
@@ -299,7 +300,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << RPL_TIME(servername, actual_time);
         break;
 	case 670:
-		std::cout << username << STRINGIFY(STARTTLS());
+		std::cout << username << RPL_STARTTLS(null);
 		break;
 	case 671:
 		std::cout << username << RPL_WHOISSECURE(user_nick);
@@ -320,7 +321,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
 		std::cout << username << RPL_LOGGEDOUT(user_nick, username, hostname, user_account);
 		break;
 	case 903:
-		std::cout << username << STRINGIFY(RPL_SASLSUCCESS());
+		std::cout << username << RPL_SASLSUCCESS(null);
 		break;
 	case 908:
 		std::cout << username << RPL_SASLMECHS(SASL_mechanisms);
@@ -344,19 +345,19 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
 		std::cout << username << ERR_TOOMANYCHANNELS(channel_name);
 		break;
 	case 406:
-		std::cout << username << STRINGIFY(ERR_WASNOSUCHNICK());
+		std::cout << username << ERR_WASNOSUCHNICK(null);
 		break;
 	case 409:
-		std::cout << username << STRINGIFY(ERR_NOORIGIN());
+		std::cout << username << ERR_NOORIGIN(null);
 		break;
 	case 417:
-		std::cout << username << STRINGIFY(ERR_INPUTTOOLONG());
+		std::cout << username << ERR_INPUTTOOLONG(null);
 		break;
 	case 421:
 		std::cout << username << ERR_UNKNOWNCOMMAND(command);
 		break;
 	case 422:
-		std::cout << username << STRINGIFY(ERR_NOMOTD());
+		std::cout << username << ERR_NOMOTD(null);
 		break;
 	case 432:
 		std::cout << username << ERR_ERRONEUSNICKNAME(user_nick);
@@ -374,19 +375,19 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
         std::cout << username << ERR_UNKNOWNERROR(command, command_info);
         break;
 	case 451:
-		std::cout << username << STRINGIFY(ERR_NOTREGISTERED());
+		std::cout << username << ERR_NOTREGISTERED(null);
 		break;
 	case 461:
 		std::cout << username << ERR_NEEDMOREPARAMS(command);
 		break;
 	case 462:
-		std::cout << username << STRINGIFY(ERR_ALREADYREGISTERED());
+		std::cout << username << ERR_ALREADYREGISTERED(null);
 		break;
 	case 464:
-		std::cout << username << STRINGIFY(ERR_PASSWDMISMATCH());
+		std::cout << username << ERR_PASSWDMISMATCH(null);
 		break;
 	case 465:
-		std::cout << username << STRINGIFY(ERR_YOUREBANNEDCREEP());
+		std::cout << username << ERR_YOUREBANNEDCREEP(null);
 		break;
 	case 471:
 		std::cout << username << ERR_CHANNELISFULL(channel_name);
@@ -407,22 +408,22 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
 		std::cout << username << ERR_BADCHANMASK(channel_name);
 		break;
 	case 481:
-		std::cout << username << STRINGIFY(ERR_NOPRIVILEGES());
+		std::cout << username << ERR_NOPRIVILEGES(null);
 		break;
 	case 482:
 		std::cout << username << ERR_CHANOPRIVSNEEDED(channel_name);
 		break;
 	case 483:
-		std::cout << username << STRINGIFY(ERR_CANTKILLSERVER());
+		std::cout << username << ERR_CANTKILLSERVER(null);
 		break;
 	case 491:
-		std::cout << username << STRINGIFY(ERR_NOOPERHOST());
+		std::cout << username << ERR_NOOPERHOST(null);
 		break;
 	case 501:
-		std::cout << username << STRINGIFY(ERR_UMODEUNKNOWNFLAG());
+		std::cout << username << ERR_UMODEUNKNOWNFLAG(null);
 		break;
 	case 502:
-		std::cout << username << STRINGIFY(ERR_USERSDONTMATCH());
+		std::cout << username << ERR_USERSDONTMATCH(null);
 		break;
 	case 524:
 		std::cout << username << ERR_HELPNOTFOUND(subject);
@@ -431,7 +432,7 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
 		std::cout << username << ERR_INVALIDKEY(target);
 		break;
 	case 691:
-		std::cout << username << STRINGIFY(ERR_STARTTLS());
+		std::cout << username << ERR_STARTTLS(null);
 		break;
 	case 696:
 		std::cout << username << ERR_INVALIDMODEPARAM(target, mode_char, parameter, description);
@@ -440,19 +441,19 @@ void ft_message(/*User &user, Server &server, Channel &channel, Message &message
 		std::cout << username << ERR_NOPRIVS(privilege);
 		break;
 	case 902:
-		std::cout << username << STRINGIFY(ERR_NICKLOCKED());
+		std::cout << username << ERR_NICKLOCKED(null);
 		break;
 	case 904:
-		std::cout << username << STRINGIFY(ERR_SASLFAIL());
+		std::cout << username << ERR_SASLFAIL(null);
 		break;
 	case 905:
-		std::cout << username << STRINGIFY(ERR_SASLTOOLONG());
+		std::cout << username << ERR_SASLTOOLONG(null);
 		break;
 	case 906:
-		std::cout << username << STRINGIFY(ERR_SASLABORTED());
+		std::cout << username << ERR_SASLABORTED(null);
 		break;
 	case 907:
-		std::cout << username << STRINGIFY(ERR_SASLALREADY());
+		std::cout << username << ERR_SASLALREADY(null);
 		break;
 
 	default:
