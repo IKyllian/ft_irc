ff7df9da31d8197eb5356189df8c1f6c5a194888
Romain Zhou <rozhou@student.42lyon.fr>
2022-06-02 14:17:50 +0200

    Send message now have correct number of parameters

--- srcs/command/away.cpp
+++ srcs/command/away.cpp
@@ -5,12 +5,12 @@ void    Server::command_AWAY(Client &sender, Message &msg)
 {
     if (msg.get_nb_parameter() == 0)
     {
-        send_message(*this, sender, ft_print_numerics(305), 305);
+        send_message(sender, ft_print_numerics(305));
         sender.set_away(false);
     }
     else
     {
-        send_message(*this, sender, ft_print_numerics(306), 306);
+        send_message(sender, ft_print_numerics(306));
         sender.set_away(true);
         sender.set_away_msg(msg.get_parameter());
     }
