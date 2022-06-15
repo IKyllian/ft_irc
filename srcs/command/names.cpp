7e357e9da6a8d921608c29e7f09430eb16eb9fb4
Romain Zhou <rozhou@student.42lyon.fr>
2022-06-15 14:33:57 +0200

    changed Names print with build :

--- srcs/command/names.cpp
+++ srcs/command/names.cpp
@@ -4,11 +4,12 @@ void Server::command_NAMES(Message &message) {
 	std::vector<std::string>		channels_string;
 	std::vector<Channel>::iterator	channel_it;
 
+
 	if (message.get_tab_parameter().size() < 1) {
 		std::cout << "TEST" << std::endl;
 		for (channel_it = _channels.begin(); channel_it != _channels.end(); channel_it++) {
 			for (std::map<Client*, std::string>::iterator it = channel_it->get_users().begin(); it != channel_it->get_users().end(); it++)
-				send_message(message.get_sender(), print_numerics(353, message.get_sender(), message.get_receiver(), &(*channel_it), &message)); //RPL_NAMREPLY (353) 
+				send_message(message.get_sender(), build_message2(353, message.get_sender(), "", &(*channel_it))); //RPL_NAMREPLY (353) 
 		}
 	} else {
 		channels_string = parse_comma(message.get_tab_parameter()[0]);
@@ -18,9 +19,9 @@ void Server::command_NAMES(Message &message) {
 			channel_it = get_channel(channels_string[i]);
 			if (channel_it == _channels.end())
 				continue ;
-			for (std::map<Client*, std::string>::iterator it = channel_it->get_users().begin(); it != channel_it->get_users().end(); it++)
-				send_message(message.get_sender(), print_numerics(353, message.get_sender(), message.get_receiver(), &(*channel_it), &message)); //RPL_NAMREPLY (353) 
+			send_message(message.get_sender(), build_message2(353, message.get_sender(), "", &(*channel_it))); //RPL_NAMREPLY (353) 
+			send_message(message.get_sender(), print_numerics(366, message.get_sender(), message.get_receiver(), &(*channel_it), &message));
 		}
 	}
-	send_message(message.get_sender(), print_numerics(366, message.get_sender(), message.get_receiver(), NULL, &message)); // RPL_ENDOFNAMES (366) 
+	// send_message(message.get_sender(), print_numerics(366, message.get_sender(), message.get_receiver(), NULL, &message)); // RPL_ENDOFNAMES (366) 
 }
