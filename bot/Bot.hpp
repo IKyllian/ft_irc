2c48e2b293bf97b102b09630b1ccacf0be4f00c5
Romain Zhou <rozhou@student.42lyon.fr>
2022-06-17 16:34:18 +0200

    Make it commpile and removed header :)

--- bot/Bot.hpp
+++ bot/Bot.hpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Bot.hpp                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/17 12:04:18 by kzennoun          #+#    #+#             */
-/*   Updated: 2022/06/17 15:43:32 by kzennoun         ###   ########lyon.fr   */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef BOT_HPP
 #define BOT_HPP
 
@@ -21,7 +9,7 @@ class Bot
 
 	void append_buffer(char* buffer);
 	std::string extract_command(size_t pos);
-	std::string	Bot::get_buffer() const;
+	std::string	get_buffer() const;
 
 	protected:
 
